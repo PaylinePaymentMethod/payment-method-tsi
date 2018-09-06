8bca34211a5675308e9c745f6bbf55dc058543eb
hmilesi <Herve.MILESI3@monext.net>
2018-09-06 11:17:20 +0200

    PAYLANO-750

---
+++ src/main/java/com/payline/payment/tsi/service/TransactionManagerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.payline.payment.tsi.service;
+
+import com.payline.payment.tsi.response.TsiStatusCheckResponse;
+import com.payline.pmapi.service.TransactionManagerService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TransactionManagerServiceImpl implements TransactionManagerService {
+
+    @Override
+    public Map<String, String> readAdditionalData(final String data, final String version) {
+        final Map<String, String> addData = new HashMap<>();
+        if (null != data) {
+            final TsiStatusCheckResponse statusCheck = new TsiStatusCheckResponse.Builder().fromJson(data);
+            addData.put("tid", statusCheck.getTid());
+            addData.put("authId", statusCheck.getAuthId());
+        }
+        return addData;
+    }
+}
