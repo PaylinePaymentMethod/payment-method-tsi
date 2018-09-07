30ce81c0b300b7b70fa5564015422e7a564d9871
hmilesi <Herve.MILESI3@monext.net>
2018-09-07 10:19:49 +0200

    PAYLSQUAD4-530

--- src/test/java/com/payline/payment/tsi/service/TransactionManagerServiceImplTest.java
+++ src/test/java/com/payline/payment/tsi/service/TransactionManagerServiceImplTest.java
@@ -11,7 +11,6 @@ public class TransactionManagerServiceImplTest {
     public void readAdditionalData() {
         final TransactionManagerServiceImpl tmsi = new TransactionManagerServiceImpl();
         final Map<String, String> addData = tmsi.readAdditionalData("{\"authId\":\"9289145\",\"tid\":\"9bc267fba6ccad33fc46a9b74411ad2b\",\"status\":\"OK\",\"ercode\":\"0\",\"message\":\"SUCCESSFUL TRANSACTION FOUND\",\"amount\":\"0,01\",\"multi\":\"f\",\"dtime\":\"2018-09-06 14:24:14\",\"country\":\"FRA\"}", "1.0");
-        Assert.assertEquals("9bc267fba6ccad33fc46a9b74411ad2b", addData.get("tid"));
         Assert.assertEquals("9289145", addData.get("authId"));
     }
 
