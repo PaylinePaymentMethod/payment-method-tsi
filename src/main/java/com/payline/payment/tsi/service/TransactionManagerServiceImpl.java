6b4069ee141627ae1bd76083b64b2df60e5fc1b6
hmilesi <Herve.MILESI3@monext.net>
2018-09-07 10:19:49 +0200

    PAYLSQUAD4-530

--- src/main/java/com/payline/payment/tsi/service/TransactionManagerServiceImpl.java
+++ src/main/java/com/payline/payment/tsi/service/TransactionManagerServiceImpl.java
@@ -1,19 +1,28 @@
 package com.payline.payment.tsi.service;
 
+import com.google.gson.JsonSyntaxException;
 import com.payline.payment.tsi.response.TsiStatusCheckResponse;
 import com.payline.pmapi.service.TransactionManagerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class TransactionManagerServiceImpl implements TransactionManagerService {
 
+    private static final Logger LOGGER = LogManager.getLogger( TransactionManagerServiceImpl.class );
+
     @Override
     public Map<String, String> readAdditionalData(final String data, final String version) {
         final Map<String, String> addData = new HashMap<>();
         if (null != data) {
-            final TsiStatusCheckResponse statusCheck = new TsiStatusCheckResponse.Builder().fromJson(data);
-            addData.put("authId", statusCheck.getAuthId());
+            try {
+                final TsiStatusCheckResponse statusCheck = new TsiStatusCheckResponse.Builder().fromJson(data);
+                addData.put("authId", statusCheck.getAuthId());
+            } catch(JsonSyntaxException e) {
+                LOGGER.error("Additional data syntax incorrect [{}]", data, e);
+            }
         }
         return addData;
     }
